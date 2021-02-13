3525b7a72931055cd98ea336e4f6203dbfccf67f
Baishali Datta <baishali95920@gmail.com>
2021-02-13 22:26:15 +0530

    first commit

---
+++ script.js
@@ -0,0 +1,88 @@
+let map;
+var coords = [];
+
+function initMap() {
+    map = new google.maps.Map(document.getElementById("map"), {
+        center: { lat: 20.5937, lng: 78.9629 },
+        zoom: 5,
+    });
+    getLocation();
+}
+
+function getLocation() {
+    if (navigator.geolocation) {
+        navigator.geolocation.getCurrentPosition(showPosition);
+    } else {
+        x.innerHTML = "Geolocation is not supported by this browser.";
+    }
+}
+
+function showPosition(position) {
+    setMarker(position.coords.latitude,position.coords.longitude, 'rec.png')
+}
+
+function submit(){    
+    const location = document.querySelector('#location').value;
+    const lat = document.querySelector('#lat').value;
+    const long = document.querySelector('#long').value;
+
+    if(!location || !lat || !long){
+        window.alert("Please enter the location and the co-ordinates.");
+        return;
+    }
+
+    if(isNaN(lat) || isNaN(long)){
+        window.alert("Please enter valid latitude and longitude.");
+        return;
+    }
+    if(coords.length>=6)
+            window.alert("You cannot add more than 5 co-oridnates.");
+    else{
+        const btn2 = document.querySelector('.button2');
+        btn2.style.backgroundColor = "#074770";
+        btn2.style.color = "#ffffff";
+        btn2.style.cursor = "pointer";
+        updateInfo(location, lat, long);
+    }
+}
+
+function updateInfo(location, lat, long){
+    var len = coords.length;
+
+    document.querySelector('#lat'+len).style.color = 'black';
+    document.querySelector('#lon'+len).style.color = 'black';
+    document.querySelector('#lat'+len).style.opacity = 1;
+    document.querySelector('#lon'+len).style.opacity = 1;
+
+    document.querySelector('#loc'+len).innerHTML = len+") "+location;
+    document.querySelector('#lat'+len).innerHTML = lat;
+    document.querySelector('#lon'+len).innerHTML = long;    
+
+    setMarker(lat,long, 'location-pin.png');
+}
+
+function setMarker(lat, long, icon){
+    const pos = { lat: parseFloat(lat), lng: parseFloat(long) };
+    const marker = new google.maps.Marker({
+        position: pos,
+        icon: icon,
+        map: map,
+    });
+    coords.push(pos);
+}
+
+
+function plot(){
+    console.log("plot",coords);
+    var line= new google.maps.Polyline({
+        path: coords,
+        geodesic: true,
+        strokeColor: '#000000',
+        strokeOpacity: 1.0,
+        strokeWeight: 2
+    });
+    
+    line.setMap(map);
+}
+
+
